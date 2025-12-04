4728ba569be9205247fd9fa88c5e9514dcd8358b
Emmanuel Ajoku <EMMANUEL.AJOKU.2024@mumail.ie>
2025-12-04 12:48:28 +0000

    Movement implemented

--- Playerstate.gd
+++ Playerstate.gd
@@ -21,7 +21,16 @@ var right_key: String = "Right"
 var up_key: String = "Up"
 var down_key: String = "Down"
 
-
+func _ready():
+	if idle_state == null or walk_state == null:
+		var state_machine = get_parent()
+		if state_machine:
+			for child in state_machine.get_children():
+				if child is PlayerIdleState and idle_state == null:
+					idle_state = child
+				elif child is PlayerWalkState and walk_state == null:
+					walk_state = child
+					
 #base 
 func process_physics(delta: float) -> State:
 	player.velocity.y += gravity * delta
--- walk.gd
+++ walk.gd
@@ -1,18 +1,24 @@
 class_name PlayerWalkState
 extends PlayerState
 
-const SPEED: float = 2
+const SPEED: float = 150.0
 func enter() -> void:
 	player.animation.play(walk_anim)
 	
 func process_physics(delta: float) -> State:
 	super(delta)
-	do_move(get_move_dir())
+	var move_dir = get_move_dir()
+	
+	if move_dir == 0:
+		return idle_state
+		
+	do_move(move_dir)
 	return null
 	
 	
 
 func get_move_dir() -> float:
+
 	return Input.get_axis(left_key,right_key)
 	
 
